5061025c00a21cdcc898549cd863077181f43c25
Steve Larsen <slarsen-3@att.net>
2020-12-05 18:00:57 -0600

    updated marker

--- map_exercise.py
+++ map_exercise.py
@@ -1,7 +1,17 @@
 import folium
+import requests
 
+
+
+
+    
 def createMap():
-    map = folium.Map(location=[38.50, -99.10], zoom_start=5)
+    map = folium.Map(location=[38.50, -99.10], zoom_start=5, tiles="Stamen Terrain")
+    
+    fg = folium.FeatureGroup(name="My Map")
+    fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a marker", icon=folium.Icon(color="green")))
+    map.add_child(fg)
+    
     map.save("E:\\mapExercise\\mapExercise.html")
 
 createMap()
